b552693b8ab4d45d7c75084649909938b524279a
Emre Çelen <79768707+emrcelen@users.noreply.github.com>
2024-02-26 23:18:28 +0300

    hello industry 👋🏻

---
+++ industry-service/src/main/java/com/wenthor/industryservice/IndustryServiceApplication.java
@@ -0,0 +1,17 @@
+package com.wenthor.industryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+public class IndustryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndustryServiceApplication.class, args);
+	}
+
+}
