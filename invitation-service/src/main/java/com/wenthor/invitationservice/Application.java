740b7d757d806f98f13a950190e228081265955e
Emre Çelen <79768707+emrcelen@users.noreply.github.com>
2024-02-27 03:09:28 +0300

    👋🏻 hello invitation

---
+++ invitation-service/src/main/java/com/wenthor/invitationservice/Application.java
@@ -0,0 +1,13 @@
+package com.wenthor.invitationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
