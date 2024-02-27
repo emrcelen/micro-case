09c909d9d9d2c36df2e6c717567dc85c9f15d1b1
Emre Çelen <79768707+emrcelen@users.noreply.github.com>
2024-02-27 03:31:34 +0300

    ✅ Business Object classes created.

---
+++ invitation-service/src/main/java/com/wenthor/invitationservice/service/mapper/InvitationServiceMapper.java
@@ -0,0 +1,43 @@
+package com.wenthor.invitationservice.service.mapper;
+
+import com.wenthor.invitationservice.model.Invitation;
+import com.wenthor.invitationservice.service.bo.InvitationBO;
+
+import java.util.List;
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
+
+public class InvitationServiceMapper {
+    public static Invitation convertToPO(InvitationBO bo){
+        return new Invitation.Builder()
+                .id(bo.id())
+                .message(bo.message())
+                .code(bo.code())
+                .status(bo.status())
+                .user(bo.user_id())
+                .createdDate(LocalDateTime.now())
+                .updatedDate(LocalDateTime.now())
+                .createdBy(bo.createdBy())
+                .updatedBy(bo.updatedBy())
+                .build();
+    }
+    public static InvitationBO convertToBO(Invitation invitation){
+        return new InvitationBO(
+                invitation.getId(),
+                invitation.getMessage(),
+                invitation.getCode(),
+                invitation.getStatus(),
+                invitation.getUser_id(),
+                invitation.getCreated(),
+                invitation.getUpdated(),
+                invitation.getCreatedBy(),
+                invitation.getUpdatedBy()
+        );
+    }
+    public static List<InvitationBO> convertToBO(List<Invitation> list){
+        return list.stream()
+                .map(InvitationServiceMapper::convertToBO)
+                .collect(Collectors.toList());
+    }
+}
