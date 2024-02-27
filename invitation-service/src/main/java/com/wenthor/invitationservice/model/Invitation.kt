e4f43cae5355020068fe8cbcbfdf1920b9edf816
Emre Çelen <79768707+emrcelen@users.noreply.github.com>
2024-02-27 04:09:46 +0300

    🔄 Invitation.kt updated.

--- invitation-service/src/main/java/com/wenthor/invitationservice/model/Invitation.kt
+++ invitation-service/src/main/java/com/wenthor/invitationservice/model/Invitation.kt
@@ -14,20 +14,21 @@ data class Invitation(
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
     val id: UUID?,
     val message: String,
-    val code: String,
+    var code: String,
     @Enumerated(EnumType.STRING)
     val status: Status,
     var user_id: UUID?,
     @Column(nullable = false, updatable = false)
     val created: LocalDateTime,
     var updated: LocalDateTime,
+    val expiredDate: LocalDateTime,
     @Column(nullable = false, updatable = false)
     val createdBy: UUID?,
     var updatedBy: UUID?
 ) {
-    constructor(): this(Builder())
+    constructor() : this(Builder())
 
-    private constructor(builder:Builder): this(
+    private constructor(builder: Builder) : this(
         builder.id,
         builder.message,
         builder.code,
@@ -35,6 +36,7 @@ data class Invitation(
         builder.user_id,
         builder.created,
         builder.updated,
+        builder.expiredDate,
         builder.createdBy,
         builder.updatedBy
     )
@@ -46,28 +48,31 @@ data class Invitation(
             private set
         var code: String = ""
             private set
-        var status:Status = Status.PENDING
+        var status: Status = Status.PENDING
             private set
         var user_id: UUID? = null
             private set
-        var created:LocalDateTime = LocalDateTime.now()
+        var created: LocalDateTime = LocalDateTime.now()
             private set
-        var updated:LocalDateTime = LocalDateTime.now()
+        var updated: LocalDateTime = LocalDateTime.now()
+            private set
+        var expiredDate: LocalDateTime = LocalDateTime.now().plusWeeks(1)
             private set
         var createdBy: UUID? = null
             private set
         var updatedBy: UUID? = null
             private set
 
-        fun id(id:UUID?) = apply { this.id = id }
+        fun id(id: UUID?) = apply { this.id = id }
         fun message(message: String) = apply { this.message = message }
         fun code(code: String) = apply { this.code = code }
         fun status(status: Status) = apply { this.status = status }
         fun user(user_id: UUID) = apply { this.user_id = user_id }
-        fun createdDate(created: LocalDateTime) = apply{this.created = created}
+        fun createdDate(created: LocalDateTime) = apply { this.created = created }
         fun updatedDate(updated: LocalDateTime) = apply { this.updated = updated }
+        fun expiredDate(expiredDate: LocalDateTime) = apply { this.expiredDate = expiredDate }
         fun createdBy(createdBy: UUID) = apply { this.createdBy = createdBy }
         fun updatedBy(updatedBy: UUID) = apply { this.updatedBy = updatedBy }
-
+        fun build() = Invitation(this)
     }
 }
